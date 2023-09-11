5a860746602ec3ef7737e35b986466fac9fd91d2
Leron <42581447+Leron33@users.noreply.github.com>
2023-09-10 20:14:33 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
-import copy
 import os
 import os.path as osp
-import numpy as np
-from scipy import sparse as sp
 import random
 
 import argparse
@@ -21,10 +17,7 @@ parser.add_argument('--num_layers', type=int, default=6)
 parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--gamma', type=float, default=1)
 parser.add_argument('--batch_size', type=int, default=512)
-parser.add_argument('--pre_epochs', type=int, default=15)
-parser.add_argument('--epochs', type=int, default=500)
-parser.add_argument('--runs', type=int, default=20)
-parser.add_argument('--test_samples', type=int, default=100)
+parser.add_argument('--epochs', type=int, default=200)
 
 args, unknown = parser.parse_known_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -35,36 +28,38 @@ def generate_y(num_nodes, truth):
     oneton = torch.arange(num_nodes)
     return [oneton, truth]
 
-def train(train_dataset, optimizer,scheduler, epoch):
+def train(train_dataset, optimizer):
     model.train()
 
     total_loss = 0
-    total_correct = 0
-    total_node = 0
+    num_examples = 0
     
-    for data in train_dataset:
+    for i in range(0, len(train_dataset), args.batch_size):
+        
+        batch = train_dataset[i:i+args.batch_size]
         optimizer.zero_grad()
+        batch_loss = 0
         
-        G1 = data[0]
-        G2 = data[1]
-        seeds = data[2]
-        truth = data[3]
-        num_nodes = G1.shape[0]
+        for data in batch:
+            optimizer.zero_grad()
         
-        Y_L, Y_total = model(G1,G2,seeds)
+            G1 = data[0]
+            G2 = data[1]
+            seeds = data[2]
+            truth = data[3]
+            num_nodes = G1.shape[0]
         
-        y = generate_y(num_nodes, truth)
-        loss = model.loss(Y_total, y)
-        correct = model.acc(Y_L, y)
+            Y_L, Y_total = model(G1,G2,seeds)
         
-        
-        total_loss += loss
-        total_correct += correct
-        total_node += num_nodes
-    total_loss.backward()
-    optimizer.step()
-    scheduler.step()
-    return total_loss.item() / total_node, total_correct/total_node
+            y = generate_y(num_nodes, truth)
+            loss = model.loss(Y_total, y)
+            batch_loss += loss
+            total_loss += loss
+            num_examples +=1
+           
+        batch_loss.backward()
+        optimizer.step()
+    return total_loss.item()/num_examples
 
 
 @torch.no_grad()
@@ -72,7 +67,7 @@ def test(test_dataset):
     model.eval()
 
     total_correct = 0
-    total_node = 0
+    num_test = 0
     for data in test_dataset:
         
         G1 = data[0]
@@ -85,9 +80,9 @@ def test(test_dataset):
         
         y = generate_y(num_nodes, truth)
         correct = model.acc(Y_L,y)
-        total_correct += correct
-        total_node += num_nodes
-    return total_correct/total_node
+        total_correct += correct/G1.shape[0]
+        num_test += 1
+    return total_correct/num_test
 
 
 def run(datasets):
@@ -104,9 +99,13 @@ def run(datasets):
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=args.gamma)
     
     for epoch in range(1, 1 + args.epochs):
-        loss, acc = train(train_dataset, optimizer,scheduler,epoch)
-        if epoch%10 == 0:
-            print(f'epoch {epoch:03d}: Loss: {loss:.4f}, Acc: {acc:.4f}')
+        loss = train(train_dataset, optimizer)
+        scheduler.step()
+        
+        if epoch%5 == 0:
+            train_acc = test(train_dataset)
+            test_acc = test(test_dataset)
+            print(f'epoch {epoch:03d}: Loss: {loss:.8f}, Training Acc: {train_acc:.4f}, Testing Acc: {test_acc:.4f}')
 
     accs = 100 * test(test_dataset)
     
@@ -121,9 +120,9 @@ if __name__ == '__main__':
     print('Preparing training data...')
     datasets = []
     graph_para = [(100,0.1,0.6,0.1),(100,0.1,0.8,0.1),(100,0.1,1,0.1),
-                 (100,0.3,0.6,0.1),(100,0.3,0.8,0.1),(100,0.3,1,0.1),
-                 (100,0.5,0.6,0.1),(100,0.5,0.8,0.1),(100,0.5,1,0.1),
-                 (100,0.1,0.6,0.1)]
+                  (100,0.3,0.6,0.1),(100,0.3,0.8,0.1),(100,0.3,1,0.1),
+                  (100,0.5,0.6,0.1),(100,0.5,0.8,0.1),(100,0.5,1,0.1),
+                  (100,0.1,0.6,0.1)]
     numgraphs = 10
     for n,p,s,theta in graph_para:
         for _ in range(numgraphs):
